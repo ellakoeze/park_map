4ec0e6f719c0c12679035f751f05365d2cc1d24d
Ella Koeze <ellakoeze@Ellas-MBP.home>
2018-11-03 16:45:05 -0400

    talkin to geoserver hell ya

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,55 @@
 import '../styles/index.scss';
+import 'leaflet-css';
+import { map, Util, geoJson } from 'leaflet';
+import { basemapLayer} from 'esri-leaflet';
 
-const L = require('leaflet');
+const $ = require('jquery');
 
-const map = L.map('map', {
-    center: [51.505, -0.09],
-    zoom: 13
+const ourMap = map('map').setView([42.9411, -85.6100], 11);
+
+basemapLayer('Gray').addTo(ourMap);
+
+var owsrootUrl = 'https://localhost:8080/geoserver/ows';
+
+var defaultParameters = {
+    service : 'WFS',
+    version : '2.0',
+    request : 'GetFeature',
+    typeName : 'cite:park',
+    outputFormat : 'text/javascript',
+    format_options : 'callback:getJson',
+    SrsName : 'EPSG:4326'
+};
+
+var parameters = Util.extend(defaultParameters);
+console.log(parameters);
+var fakeurl = owsrootUrl + Util.getParamString(parameters);
+console.log(fakeurl);
+
+var URL = 'http://localhost:8080/geoserver/cite/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cite%3Apark&maxFeatures=50&outputFormat=text/javascript&format_options=callback%3AgetJson'
+
+var WFSLayer = null;
+var ajax = $.ajax({
+    url : URL,
+    dataType : 'jsonp',
+    jsonpCallback : 'getJson',
+    success : (response) =>{
+    	console.log('did this work', response);
+        WFSLayer = geoJson(response, {
+            // style: function (feature) {
+            //     return {
+            //         stroke: false,
+            //         fillColor: 'FFFFFF',
+            //         fillOpacity: 0
+            //     };
+            // },
+            // onEachFeature: function (feature, layer) {
+            //     popupOptions = {maxWidth: 200};
+            //     layer.bindPopup("Popup text, access attributes with feature.properties.ATTRIBUTE_NAME"
+            //         ,popupOptions);
+            // }
+        }).addTo(ourMap);
+    }
 });
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiZWxsYWsiLCJhIjoiY2o3NzF2NWg3MTVjeTMzbnhsMXF1bzg2ZCJ9.ynl5jTicJ4QLLE070PB-YQ'
-}).addTo(map);
-
-console.log('webpack starterkit');
-console.log('can i console');
-
-
-
-
-
 
 
