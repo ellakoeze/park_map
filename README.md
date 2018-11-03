4b321ebb11c728b460b0a91707d7e5db5d1bba9a
Ella Koeze <ellakoeze@Ellas-MBP.home>
2018-11-03 14:49:01 -0400

    webpack set up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# park_map
+# Park map projects
+
+home for my geog 777 park mobile map project
---
+++ src/scripts/index.js
@@ -0,0 +1,5 @@
+import '../styles/index.scss';
+
+console.log('webpack starterkit');
+console.log('can i console');
+
