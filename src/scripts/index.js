2eab4c2998c988cfa22e8e37ca7371e8f5e30a28
Ella Koeze <ellakoeze@Ellas-MBP.home>
2018-11-03 15:15:58 -0400

    leaflet map

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,25 @@
 import '../styles/index.scss';
 
+const L = require('leaflet');
+
+const map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 13
+});
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiZWxsYWsiLCJhIjoiY2o3NzF2NWg3MTVjeTMzbnhsMXF1bzg2ZCJ9.ynl5jTicJ4QLLE070PB-YQ'
+}).addTo(map);
+
 console.log('webpack starterkit');
 console.log('can i console');
+
+
+
+
+
+
 
