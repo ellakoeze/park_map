98b32ffd432a064a6b3fb2ff7abe9287a95cbba9
Ella Koeze <ellakoeze@Ellas-MBP.home>
2018-11-03 21:42:43 -0400

    ajax is working

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,55 +1,64 @@
 import '../styles/index.scss';
 import 'leaflet-css';
-import { map, Util, geoJson } from 'leaflet';
+import { map, Util, geoJson, Icon } from 'leaflet';
 import { basemapLayer} from 'esri-leaflet';
 
 const $ = require('jquery');
 
-const ourMap = map('map').setView([42.9411, -85.6100], 11);
+const ourMap = map('map').setView([42.9411, -85.6100], 13);
 
 basemapLayer('Gray').addTo(ourMap);
 
-var owsrootUrl = 'https://localhost:8080/geoserver/ows';
+// var owsrootUrl = 'https://localhost:8080/geoserver/ows';
 
-var defaultParameters = {
-    service : 'WFS',
-    version : '2.0',
-    request : 'GetFeature',
-    typeName : 'cite:park',
-    outputFormat : 'text/javascript',
-    format_options : 'callback:getJson',
-    SrsName : 'EPSG:4326'
-};
+// var defaultParameters = {
+//     service : 'WFS',
+//     version : '2.0',
+//     request : 'GetFeature',
+//     typeName : 'cite:park',
+//     outputFormat : 'text/javascript',
+//     format_options : 'callback:getJson',
+//     SrsName : 'EPSG:4326'
+// };
 
-var parameters = Util.extend(defaultParameters);
-console.log(parameters);
-var fakeurl = owsrootUrl + Util.getParamString(parameters);
-console.log(fakeurl);
+// Icon.Default.imagePath = '.';
+// OR
+delete Icon.Default.prototype._getIconUrl;
 
-var URL = 'http://localhost:8080/geoserver/cite/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cite%3Apark&maxFeatures=50&outputFormat=text/javascript&format_options=callback%3AgetJson'
-
-var WFSLayer = null;
-var ajax = $.ajax({
-    url : URL,
-    dataType : 'jsonp',
-    jsonpCallback : 'getJson',
-    success : (response) =>{
-    	console.log('did this work', response);
-        WFSLayer = geoJson(response, {
-            // style: function (feature) {
-            //     return {
-            //         stroke: false,
-            //         fillColor: 'FFFFFF',
-            //         fillOpacity: 0
-            //     };
-            // },
-            // onEachFeature: function (feature, layer) {
-            //     popupOptions = {maxWidth: 200};
-            //     layer.bindPopup("Popup text, access attributes with feature.properties.ATTRIBUTE_NAME"
-            //         ,popupOptions);
-            // }
-        }).addTo(ourMap);
-    }
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
+
+const layers = ['park', 'poi', 'roads', 'trails', 'water']
+
+$.each(layers, function (i, item) {
+
+  console.log(i, item);
+	let URL = `http://localhost:8080/geoserver/park/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=park:${item}&maxFeatures=50&outputFormat=application/json`
+	
+	let data = getData(URL);
+
+	data.then((result)=>{
+		geoJson(result).addTo(ourMap);
+	})
+
+});
+
+async function getData(url) {
+    let result;
+
+    try {
+        result = await $.ajax({
+            url: url,
+            type: 'GET'
+        });
+
+        return result;
+    } catch (error) {
+        console.error('my request errored', error);
+    }
+}
 
 
