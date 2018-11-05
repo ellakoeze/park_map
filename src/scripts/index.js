a0724382d964e77b0f32b3efaa62fef116248a74
Ella Koeze <ellakoeze@Ellas-MBP.home>
2018-11-04 22:01:44 -0500

    users can dadd to map

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -23,7 +23,7 @@ const $ = require('jquery');
 //instantiate the map
 const ourMap = map('map');
 var userLocation = null;
-var radius = 1500000;
+var radius = 15000000;
 var locationCount = 1;
 
 //map view centered on reeds lake
@@ -70,6 +70,7 @@ function init(){
   		var geoJsonLayer;
 
       if (userLocation && result.features[0].geometry.type == 'Point'){
+        console.log(result);
         geoJsonLayer = geoJson(result, {filter: distanceCheck}).addTo(allLayers);
       }
       else{
@@ -174,6 +175,82 @@ function distanceCheck(feature){
   }
 
 }
+
+
+
+//pushign to database — steps
+
+//create a popup form (look at leaflet docs)
+//'<form role="form" id="form" onsubmit="return addMarker();">'+
+
+var newMark = null;
+
+let popupContent = '<button id="popUpButton">Add location to map</button> </br><button id="removeButton">Remove</button> ';
+
+ourMap.on('click touchstart', function(e){
+    newMark = new marker(e.latlng).addTo(ourMap);
+    newMark.bindPopup(popupContent).openPopup();
+    var rmvButton = document.getElementById('removeButton').addEventListener("touchstart", removeButton);
+    var addButton = document.getElementById('popUpButton').addEventListener("touchstart", getInfo);
+});
+
+
+function removeButton(){
+  if(newMark){
+    ourMap.removeLayer(newMark);
+  }
+};
+
+function getInfo(){
+  ///make a form do something for user input here
+  console.log(newMark);
+  var postData = 
+      '<wfs:Transaction service="WFS" version="1.0.0"'
+      +'  xmlns:wfs="http://www.opengis.net/wfs"'
+      +'  xmlns:gml="http://www.opengis.net/gml"'
+      +'  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
+      +'  xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.0.0/WFS-transaction.xsd http://localhost:8080/geoserver/wfs/DescribeFeatureType?typename=park:poi">'
+      +'  <wfs:Insert>'
+      +'    <poi>'
+      +'      <geom>'
+      +'         <gml:Point srsDimension="2" srsName="urn:x-ogc:def:crs:EPSG:4326">\n'
+     + '          <gml:coordinates decimal="." cs="," ts=" ">' + newMark._latlng.lat + ',' + newMark._latlng.lng+ '</gml:coordinates>\n'
+     + '        </gml:Point>\n'
+      +'      </geom>'
+      +'      <type>testType</type>'
+      +'      <name>testName</name>'
+      +'    </poi>'
+      +'  </wfs:Insert>'
+      +'</wfs:Transaction>';
+
+  sendData(postData);
+}
+
+
+
+
+function sendData(data){
+    //let getURL = `http://localhost:8080/geoserver/park/ows?service=WFS&version=1.0.0&request=DescribeFeatureType&outputFormat=application/json`
+
+    let postURL = `http://localhost:8080/geoserver/park/ows`
+   //Get the data from the form & send it to the service.
+  $.ajax({
+    type: "POST",
+    url: postURL,
+    dataType: "xml",
+    contentType: "text/xml",
+    data: data,
+    //TODO: Error handling
+    success: function(xml) {  
+      console.log(xml);
+    }
+  });
+
+   return false; //Don't submit anything
+}   
+
+
+
 
 
 init();
